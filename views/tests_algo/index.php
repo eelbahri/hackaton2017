b4ea3f2a253fd800a15d8e9659d193d5a483fe6b
Cyriaque MALDAT <cmaldat@gmail.com>
2017-04-26 17:28:55 +0200

    Inialisation Symfony 3

--- views/tests_algo/index.php
+++
@@ -1,9 +0,0 @@
-<h1>Quel est votre niveau en Algo ?</h1>
-
-<a href="/<?php echo FOLDERNAME; ?>/testsAlgo/facilePremier">
-    <button class="btn btn-default">Facile</button>
-</a>
-
-<a href="/<?php echo FOLDERNAME; ?>/testsAlgo/NormalPremier">
-    <button class="btn btn-default">Normal</button>
-</a>
